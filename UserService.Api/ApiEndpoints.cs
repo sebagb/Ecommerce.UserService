2cbd0410a715dffeb272c3535324f77de832bd28
sebagb <sgbcmpu@gmail.com>
2025-10-09 10:21:11 +0200

    Move api endpoint routing to designated class

--- UserService.Api/ApiEndpoints.cs
+++ UserService.Api/ApiEndpoints.cs
@@ -1,10 +1,85 @@
+using System.Text.Json;
+using UserService.Contract.Requests;
+
 namespace UserService.Api;
 
 public static class ApiEndpoints
 {
     private const string Base = "users";
-    public static string Create { get; } = $"{Base}";
-    public static string GetByCredentials { get; } = $"{Base}";
-    public static string GetById { get; } = $"{Base}/{{id}}";
-    public static string Update { get; } = $"{Base}";
+    private static string Create { get; } = $"{Base}";
+    private static string GetByCredentials { get; } = $"{Base}";
+    private static string GetById { get; } = $"{Base}/{{id}}";
+    private static string Update { get; } = $"{Base}";
+
+    public static void RegisterUserEndpoints(this IEndpointRouteBuilder builder)
+    {
+        builder.MapPost(Create, (Delegate)CreateUser);
+        builder.MapGet(GetByCredentials, GetUserByCredentials);
+        builder.MapGet(GetById, GetUserById);
+        builder.MapPut(Update, UpdateUser);
+    }
+
+    private static async Task<IResult> CreateUser(
+        HttpContext context)
+    {
+        CreateUserRequest? createUserRequest;
+        try
+        {
+            createUserRequest =
+                await context.Request.ReadFromJsonAsync<CreateUserRequest>();
+        }
+        catch (JsonException)
+        {
+            return Results.BadRequest("Failed to deserialize request body");
+        }
+
+        var user = createUserRequest!.MapToUser();
+
+        //var isCreated = repo.Create(user);
+
+        var userResponse = user.MapToResponse();
+        return Results.Ok(userResponse);
+    }
+
+    private static IResult GetUserByCredentials(HttpRequest request)
+    {
+        var credentials = request.Query.MapToGetUserByCredentialsRequest();
+        var username = credentials.Username;
+        var password = credentials.Password;
+
+        //var user = repo.GetUserByCredentials(username, password)
+        //var response = user.MapToResponse();
+        var response = "user.MapToResponse()";
+        return Results.Ok(response);
+    }
+
+    private static IResult GetUserById(int id)
+    {
+        // var user = repo.GetUserById(id);
+        // var response = user.MapToResponse();
+
+        return Results.Ok($"Get by Id: {id}");
+    }
+
+    private static async Task<IResult> UpdateUser(HttpRequest request)
+    {
+        UpdateUserRequest? updateUserRequest;
+        try
+        {
+            updateUserRequest =
+                await request.ReadFromJsonAsync<UpdateUserRequest>();
+        }
+        catch (JsonException)
+        {
+            return Results.BadRequest("Failed to deserialize request body");
+        }
+
+        var id = Guid.NewGuid(); // take from jwt
+        var user = updateUserRequest.MapToUser(id);
+
+        // var isUpdated = repo.Update(user)
+
+        var response = user.MapToResponse();
+        return Results.Ok(response);
+    }
 }
--- UserService.Api/Program.cs
+++ UserService.Api/Program.cs
@@ -1,6 +1,4 @@
-using System.Text.Json;
 using UserService.Api;
-using UserService.Contract.Requests;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,67 +10,6 @@ app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
-app.MapPost(ApiEndpoints.Create, async (HttpContext context) =>
-{
-    var createUserRequest = (CreateUserRequest?)null;
-    try
-    {
-        createUserRequest =
-            await context.Request.ReadFromJsonAsync<CreateUserRequest>();
-    }
-    catch (JsonException)
-    {
-        return Results.BadRequest("Failed to deserialize request body");
-    }
-
-    var user = createUserRequest!.MapToUser();
-
-    // var isCreated = repo.Create(user);
-
-    var userResponse = user.MapToResponse();
-    return Results.Ok(userResponse);
-});
-
-app.MapGet(ApiEndpoints.GetByCredentials, (HttpRequest request) =>
-{
-    var credentials = request.Query.MapToGetUserByCredentialsRequest();
-    var username = credentials.Username;
-    var password = credentials.Password;
-
-    //var user = repo.GetUserByCredentials(username, password)
-    //var response = user.MapToResponse();
-    var response = "user.MapToResponse()";
-    return Results.Ok(response);
-});
-
-app.MapGet(ApiEndpoints.GetById, (int id) =>
-{
-    // var user = repo.GetUserById(id);
-    // var response = user.MapToResponse();
-
-    return Results.Ok($"Get by Id: {id}");
-});
-
-app.MapPut(ApiEndpoints.Update, async (HttpRequest request) =>
-{
-    var updateUserRequest = (UpdateUserRequest?)null;
-    try
-    {
-        updateUserRequest =
-            await request.ReadFromJsonAsync<UpdateUserRequest>();
-    }
-    catch (JsonException)
-    {
-        return Results.BadRequest("Failed to deserialize request body");
-    }
-
-    var id = Guid.NewGuid(); // take from jwt
-    var user = updateUserRequest.MapToUser(id);
-
-    // var isUpdated = repo.Update(user)
-
-    var response = user.MapToResponse();
-    return Results.Ok(response);
-});
+app.RegisterUserEndpoints();
 
 app.Run();
